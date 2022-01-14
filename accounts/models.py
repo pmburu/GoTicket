8ddd320f64c5e7b3fdd48d8f01348a302e83d381
Peter Mburu <6551883+pmburu@users.noreply.github.com>
2022-01-14 15:44:38 +0100

    User - Custom User + User Types

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,81 @@
+import uuid
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+# Import custom manager classes
+from .managers import (
+UserTypes, CustomerManager, EventManagerManager
+)
+
+'''
+Custom user model inheriting from the abtract user since
+there is no major Django default user settings being changed.
+
+For reference see:
+1. https://bit.ly/3zWfZ8M
+2. https://bit.ly/34RB6Og
+'''
+
+class User(AbstractUser):
+
+    # Default user type --> invoked upon registration and updated on choices
+    base_type = UserTypes.CUSTOMER
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_type = models.CharField(
+        _('User Type'), max_length=13, choices=UserTypes.choices, default=base_type
+    )
+
+    def get_absolute_url(self):
+        return reverse('users:detail', kwargs={'username': self.username})
+
+    '''
+    Save changes made to the user 'upon registration' by invoking the
+    parent save method that is shipped with the Django AbstractUser class.
+    '''
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.type = self.base_type
+        return super().save(*args, **kwargs)
+
+class EventManagerDetails(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='event_manager_details')
+    # tickets_sold = models.ForeignKey(Tickets, on_delete=models.PROTECT,  related_name='tickets_sold_by') -- awaiting the tickets model to be created
+
+class Manager(User):
+
+    base_type = UserTypes.EVENT_MANAGER
+    objects = EventManagerManager()
+
+    @property
+    def manager_details(self):
+        return self.eventmanagerdetails
+
+    '''
+    Tell Django database engine to treat this class as a proxy model.
+    No table created, hence number of queries are reduced, perfomance increased
+    '''
+
+    class Meta:
+        proxy = True
+
+class Customer(User):
+
+    base_type = UserTypes.CUSTOMER
+    objects = CustomerManager()
+
+    '''
+    Tell Django database engine to treat this class as a proxy model.
+    No table created, hence number of queries are reduced, perfomance increased
+    '''
+
+    class Meta:
+        proxy = True
+
+'''
+More about proxy models can be found here:
+1. https://bit.ly/3Gvp7nw
+2. https://bit.ly/3zVlXHa
+'''
--- goticket/settings.py
+++ goticket/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+# Import timedelta and settings in order for simplejwt to reference datetime
+from datetime import timedelta
+from django.conf import settings
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,10 +42,21 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Django based applications
+    'accounts',
+
     # ThirdParty helper applications 'libraries'
     'rest_framework',
     'djoser'
 ]
+
+'''
+Custom user models require Django to be informed where to point
+to when referencing anything user. Hence, we name it as:
+application_name.class
+'''
+
+AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -143,12 +158,44 @@ REST_FRAMEWORK = {
 
 '''
 Configure django-rest-framework-simplejwt to
-use the Authorization: JWT <access_token> header:
+use the Authorization: Bearer <access_token> header:
+
+reference:
+1. https://bit.ly/3rlCfFg
 '''
 
 SIMPLE_JWT = {
-   'AUTH_HEADER_TYPES': ('JWT',),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': settings.SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
+
 
 '''
 Djoser settings
