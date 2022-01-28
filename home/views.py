5e08ccea09063c6e944f953d68293cacfb65210d
VladyslavKT <3677112+Vladislolve@users.noreply.github.com>
2022-01-28 08:42:44 +0100

    Login auth and added some relative paths

--- home/views.py
+++ home/views.py
@@ -5,8 +5,19 @@ In other frameworks you may also find conceptually similar
 implementations named something like 'Resources' or 'Controllers'.
 '''
 
-from rest_framework import viewsets
 from django.shortcuts import render
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+  
+  
+class HelloView(APIView):
+    permission_classes = (IsAuthenticated, )
+  
+    def get(self, request):
+        content = {'message': 'Hello, GeeksforGeeks'}
+        return Response(content)
 
 def homepage(request):
 	return render(request, 'index.html')
